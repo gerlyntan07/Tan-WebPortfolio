5382d7130557d3cdab4200c14d7beb931d1fd7a6
Gerlyn Tan <gerlyntan07@gmail.com>
2024-10-04 22:39:47 +0800

    Closing menu when clicking a nav button

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,10 @@ function App() {
 
                         <div className='popup-navBtn'>
                             <ul>
-                                <li><a href="#hero">Home</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Projects</a></li>
-                                <li><a href="#">Contact</a></li>
+                                <li><a href="#hero" onClick={closeMenu}>Home</a></li>
+                                <li><a href="#" onClick={closeMenu}>About</a></li>
+                                <li><a href="#" onClick={closeMenu}>Projects</a></li>
+                                <li><a href="#" onClick={closeMenu}>Contact</a></li>
                             </ul>
                         </div>
                     </div>
