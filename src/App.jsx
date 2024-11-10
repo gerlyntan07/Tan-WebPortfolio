2c445acde9f963d3b8d80e98945f08120226380a
Gerlyn Tan <gerlyntan07@gmail.com>
2024-11-10 22:49:32 +0800

    Added sitemap

--- src/App.jsx
+++ src/App.jsx
@@ -168,7 +168,7 @@ function App() {
         }
     }
 
-    {/* SEO */}
+    {/* SEO */ }
     const structuredData = {
         "@context": "https://schema.org",
         "@type": "Person",
@@ -191,10 +191,10 @@ function App() {
     return (
         <>
             <Helmet>
-                <script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-                />
+                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
+                <title>Gerlyn Tan | Portfolio</title>
+                <meta name="description" content="Portfolio of a front-end developer and UI designer specializing in React.js." />
+                <meta name="robots" content="index, follow" />
             </Helmet>
 
             <div className={`bodyContainer ${darkMode ? 'darkBody' : ''}`}>
