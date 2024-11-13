fcd2801a250ae5da7a3d044febe743e713653071
Gerlyn Tan <gerlyntan07@gmail.com>
2024-11-13 21:08:31 +0800

    Prevented sending another contact form when a form is still sending

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,15 +3,17 @@ import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const ContactForm = ({darkMode}) => {
+export const ContactForm = ({ darkMode }) => {
     const form = useRef();
+    const [loading, setLoading] = useState(false);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [msg, setMsg] = useState('');
 
     const sendEmail = (e) => {
         e.preventDefault();
-                
+        setLoading(true);
+
         emailjs
             .sendForm('service_lvaypj2', 'template_n1qgo2v', form.current, {
                 publicKey: 'Z9nUoyrtbdrLWu_kT',
@@ -20,17 +22,19 @@ export const ContactForm = ({darkMode}) => {
                 () => {
                     toast.success('Message sent! I will get back to you through the email account you sent as soon as possible.', {
                         position: 'top-center',
-                        autoClose: 5000, // Close after 3 seconds
+                        autoClose: 5000,
                     });
                     setName('');
                     setEmail('');
                     setMsg('');
+                    setLoading(false); 
                 },
                 (error) => {
                     toast.error(`Failed to send email: ${error}`, {
                         position: 'bottom-right',
-                        autoClose: 5000, // Close after 5 seconds
-                    });                    
+                        autoClose: 5000,
+                    });
+                    setLoading(false);
                 }
             );
     };
@@ -38,13 +42,16 @@ export const ContactForm = ({darkMode}) => {
     return (
         <>
             <form ref={form} onSubmit={sendEmail} className='contactFormContainer'>
-                <label className={`contactLbl ${darkMode ? 'darkTextColor': ''}`}>Name</label>
-                <input className={`contactInput ${darkMode ? 'darkContactInput' : 'lightContactInput'}`} type="text" value={name} name="from_name" placeholder='Your name' onChange={(e) => setName(e.target.value)} required />
-                <label className={`contactLbl ${darkMode ? 'darkTextColor': ''}`}>Email</label>
-                <input className={`contactInput ${darkMode ? 'darkContactInput' : 'lightContactInput'}`} type="email" value={email} name="from_email" placeholder='Your email' onChange={(e) => setEmail(e.target.value)} required />
-                <label className={`contactLbl ${darkMode ? 'darkTextColor': ''}`}>Message</label>
+                <label className={`contactLbl ${darkMode ? 'darkTextColor' : ''}`}>Name</label>
+                <input className={`contactInput ${darkMode ? 'darkContactInput' : 'lightContactInput'}`} type="text" value={name} name="from_name" placeholder="Your name" onChange={(e) => setName(e.target.value)} required />
+                
+                <label className={`contactLbl ${darkMode ? 'darkTextColor' : ''}`}>Email</label>
+                <input className={`contactInput ${darkMode ? 'darkContactInput' : 'lightContactInput'}`} type="email" value={email} name="from_email" placeholder="Your email" onChange={(e) => setEmail(e.target.value)} required />
+                
+                <label className={`contactLbl ${darkMode ? 'darkTextColor' : ''}`}>Message</label>
                 <textarea className={`contactInput-txtArea ${darkMode ? 'darkContactInput' : 'lightContactInput'}`} name="message" value={msg} onChange={(e) => setMsg(e.target.value)} required />
-                <input className='submitContactForm' type="submit" value="SUBMIT" />
+                
+                <input className="submitContactForm" type="submit" value={loading ? 'SENDING...' : 'SUBMIT'} disabled={loading} />
             </form>
             <ToastContainer theme={darkMode ? 'dark' : 'light'} newestOnTop={true} hideProgressBar={true} />
         </>
