2c82af2e7b656e4cab47db779904ed84a18d07b5
Gerlyn Tan <gerlyntan07@gmail.com>
2024-11-13 21:22:56 +0800

    Uninstalled speedInsights of Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import "./index.css";
 import App from "/src/App.jsx";
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <SpeedInsights>
-        <HelmetProvider>
-            <App />
-        </HelmetProvider>
-    </SpeedInsights>
+    <HelmetProvider>
+        <App />
+    </HelmetProvider>
 );
