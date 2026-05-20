30f08516830d68d91b4e29dcb0894e0e65c97313
Gerlyn Tan <gerlyntan07@gmail.com>
2026-05-20 19:07:13 +0800

    redesign about me card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,16 @@ function About() {
     <main className="flex justify-center items-center w-full py-15">
       <div className="w-[90%] flex flex-col justify-start items-center">
         <p className="text-left w-full font-mono text-tertiary">ABOUT ME</p>
-        <TiltCard image={image} title="My Card" className="w-full">
-          <p>This is the content inside the tilt card.</p>
+        <TiltCard image={image} title="FULL-STACK DEVELOPER">
         </TiltCard>
 
         <div className="w-full flex flex-col">
-          <p className="font-mono text-accent">ABOUT ME</p>
+          <p className="font-mono text-accent hidden">ABOUT ME</p>
+          <p className="font-serif text-2xl">Hi! I'm Gerlyn.</p>
+          <p className="font-mono text-xs text-darkgray my-4">full-stack developer</p>
+          <p className="font-google text-sm text-black/70">Grew up in Las Piñas specializing in React, React Native, and Python backend development.</p>
+          <p className="font-google text-sm text-black/70 my-2">I build scalable web, mobile, and embedded solutions—from cloud-deployed business platforms to AI-powered applications. My work includes deploying production systems on Amazon Web Services, integrating payment systems with Adyen, and developing hardware-connected applications using Raspberry Pi Foundation devices.</p>
+          <p className="font-google text-sm text-black/70">I’m passionate about creating software that is not only functional, but secure, scalable, and ready for real-world use.</p>
         </div>
       </div>
     </main>
--- src/components/ui/tiltcard.jsx
+++ src/components/ui/tiltcard.jsx
@@ -37,29 +37,31 @@ export function TiltCard({ image, title, children }) {
           rotateX,
           rotateY,
           transformStyle: "preserve-3d",
-          borderRadius: "16px",
           overflow: "hidden",
           width: "280px",
           height: "360px",
-          cursor: "pointer",
+          cursor: "pointer",          
         }}
+        className="px-3 pt-3 bg-linear-to-r from-violet-100 via-white to-orange-50 rounded-2xl border-2 border-gray-300 mt-3 mb-8"
         whileHover={{ scale: 1.03 }}
+        
       >
         <img
           src={image}
           alt={title}
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          className="rounded-lg"
         />
         {/* Content elevated in 3D */}
         <div
           style={{
             position: "absolute",
             bottom: 0,
-            padding: "1rem",
-            transform: "translateZ(40px)",  /* pops forward */
-            color: "white",
+            transform: "translateZ(40px)",  /* pops forward */            
             fontWeight: 500,
+            width: "100%",            
           }}
+          className="font-mono bg-linear-to-r from-violet-100 via-white to-orange-50 h-10 text-sm text-darkgray"
         >
           {title}
         </div>
